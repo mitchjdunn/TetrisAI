837871b872785c17af7b298c699994391295bbd0
Maku <makucross@gmail.com>
2017-02-25 15:15:36 -0500

    Boxes now know if they are checked or not

--- display.py
+++ display.py
@@ -10,31 +10,17 @@ class Display:
 		
 		Label(master, text = "Play Tetris!\n\n").pack()
 		
-		self.gameGrid = Tk()
+		self.gameGrid = GameGrid(self)
 		
 		self.buttons = {"down":Button(master,text="down",command=self.downPressed),
 			"left":Button(master,text="left",command=self.leftPressed),
 			"right":Button(master,text="right",command=self.rightPressed),
 			"endTurn":Button(master,text="endTurn",command=self.endTurn),
-			"printGrid":Button(master,text="printGrid",command=self.printGrid)}
+			"printGrid":Button(master,text="printGrid",command=self.gameGrid.printGrid)}
 		for key in self.buttons:
 			self.buttons[key].pack()
-		#I'm making the boxes checkbuttons to help with debugging
-		self.checkVars = [[None for col in range(self.board.width)] for row in range(self.board.height)]#I think checkbuttons needs this
-		self.checks = [[None for col in range(self.board.width)] for row in range(self.board.height)]
-		for row in range(0,self.board.height):
-			for col in range(0,self.board.width):
-				self.checkVars[row][col] = IntVar()
-				self.checks[row][col] = Checkbutton(self.gameGrid,variable=self.checkVars[row][col])
-				#self.checks[row][col].var = self.checkVars[row][col]
-				self.checks[row][col].grid(row=row,column=col)
 			
 		mainloop()
-	def switchVar(self,row,col):
-		print "switching."
-		print " before: ",self.checkVars[row][col].get()
-		self.checkVars[row][col] = int((not (self.checkVars[row][col].get())))
-		print " after: ",self.checkVars[row][col]
 	def downPressed(self):
 		print "The user has pressed down"
 	def leftPressed(self):
@@ -44,30 +30,38 @@ class Display:
 	def endTurn(self): #This is called when a piece lands at the bottom
 		#This is not yet optimized. It is only for testing.
 		for row in range(0,self.board.height):
-			if self.checkFullRow(row):
+			if self.gameGrid.checkFullRow(row):
 				print "YE A ROW IS FULL"
+class GameGrid:
+	def __init__(self,father,master=Tk()):
+		self.master = master
+		self.father = father
+		self.boxes = [[Box(self.master) for col in range(self.father.board.width)] for row in range(self.father.board.height)]
+		for row in range(0,self.father.board.height):
+			for col in range(0,self.father.board.width):
+				self.boxes[row][col].grid(row,col)
+				
 	def checkFullRow(self,row):
-		#rowChecks = []
-		#for col in range(0,self.board.width):
-		#	rowChecks.append(self.checks[row][col])
-		#rowChecks = [self.checks[row][col] for col in range(0,self.board.width)]
-		#rowBools = [check.variable() for check in rowChecks]
 		for intVar in self.checkVars[row]:
 			intVal = intVar.get()
 			if intVal==1:
 				return 1
 		return 0
-		#if 0 in [self.checkVars[row].get()]: return 0
-		#else: return 1
-		#for val in self.checkVars[row]:
-		#	print val.get()
 	def printGrid(self):
-		for row in range(0,self.board.height):
-			#for col in range(0,self.board.width):
-				#iv = self.checkVars[row][col]
-				#print iv.get()
-				#check = self.checks[row,col]
-				#check.toggle()
-			#print [intVar.get() for intVar in self.checkVars[row]]
-			print [che.get() for che in self.checkVars[row]]
+		for row in range(0,self.father.board.height):
+			print [box.get() for box in self.boxes[row]]
 		print "\n\n"
+class Box:
+	def __init__(self,master):
+		self.intVar = IntVar()
+		self.isChecked = False #This is stupid, but I've tried for HOURS getting the other way to work and it won't. LIterally hours. 2/25/17 from 8am to 3:13pm.
+		self.master = master
+		self.checkBox = Checkbutton(self.master,variable=self.intVar,command=self.hitBox)
+		self.checkBox.var = self.intVar
+	def get(self):
+		return self.isChecked
+	def grid(self,row,col):
+		self.checkBox.grid(row=row,column=col)
+	def hitBox(self):
+		#print "You hit a box. Good for you."
+		self.isChecked = not self.isChecked
--- test.py
+++ test.py
@@ -4,14 +4,21 @@ from Tkinter import *
 
 class Test:
 	def __init__(self):
-		self.master=Tk()
-		self.checkVar = IntVar()
-		self.check = Checkbutton(self.master,variable=self.checkVar)
-		self.check.grid(row=0,column=0)
-		self.printCheck = Button(self.master,text="PrintIt",command=self.printIt)
-		self.printCheck.grid(row=1,column=0)
+		master=Tk()
+		self.check = [None,None]
+		chevas = []
+		for index in range(0,2):
+			print "hi"
+			#self.checkVar = IntVar()
+			chevas.append(IntVar())
+			newVa = chevas[len(chevas)-1]
+			self.check[index] = Checkbutton(master,variable=newVa)
+			self.check[index].grid(row=0,column=index)
+			self.check[index].var = newVa
+		Button(master,text="PrintIt",command=self.printIt).grid(row=1,column=0)
 		mainloop()
 	def printIt(self):
-		print self.checkVar.get()
+		print "0: ",self.check[0].var.get()
+		print "1: ",self.check[1].var.get()
 t = Test()
 print "hi"
