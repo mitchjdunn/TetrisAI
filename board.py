74edf4384ac4a9c6302965c79a750defea41dd56
Maku <makucross@gmail.com>
2017-02-25 16:51:16 -0500

    Added the ability to add tetros

--- board.py
+++ board.py
@@ -1,7 +1,32 @@
 class Board:
-	def __init__(self,height=20,width=10):
-		self.height = height
+	def __init__(self,depth=20,width=10):
+		self.depth = depth
 		self.width = width
+		self.tetroStartPoint=(self.width/2,0)
 class Tetro:#"Tetronimo" is the name of the combination of four blocks
-	def __init__(self, type):
+	def __init__(self, type, board):
 		self.type = type #Type can be J,L,O,T,S,Z,I
+		self.board = board
+		self.spaces = self.getStartBoxPointList()#This will change upon movement
+		#Should the tetro hold a list of the dimensions of every box in it?
+		
+		
+		
+		
+	def getStartBoxPointList(self): #start is the placement of the highest block (or leftmost highest)
+		start = self.board.tetroStartPoint
+		if self.type == "J":
+			return [[start[0],start[1]],[start[0],start[1]+1],[start[0]+1,start[1]+1],[start[0]+2,start[1]+1]]
+		if self.type == "L":
+			return [[start[0],start[1]],[start[0],start[1]+1],[start[0]-1,start[1]+1],[start[0]-2,start[1]+1]]
+		if self.type == "O":
+			return [[start[0],start[1]],[start[0]+1,start[1]],[start[0],start[1]+1],[start[0]+1,start[1]+1]]
+		if self.type == "T":
+			return [[start[0],start[1]],[start[0],start[1]+1],[start[0]-1,start[1]+1],[start[0]+1,start[1]+1]]
+		if self.type == "S":
+			return [[start[0],start[1]],[start[0]+1,start[1]],[start[0],start[1]+1],[start[0]-1,start[1]+1]]
+		if self.type == "Z":
+			return [[start[0],start[1]],[start[0]+1,start[1]],[start[0],start[1]+1],[start[0]-1,start[1]+1]]
+		if self.type == "I":
+			return [[strart[0],start[1]],[start[0]+1,start[1]],[start[0]+2,start[1]],[start[0]+3,start[1]]]
+		print "LETTER MATCHES NO KNOWN SHAPE!"
--- display.py
+++ display.py
@@ -1,10 +1,16 @@
 import Tkinter
 from Tkinter import *
-from board import Board
+from board import *
+#Note: A "Box" is just a space. A "block" is a box that holds a piece. A "Tetro" is one of the four-blocked pieces in a Tetris game.
+#I need to work on the dimensions. I think I"m confusing rows and columns. It can either be [column,row] or [row][column]
+#Rows should count down.
 class Display:
 	def __init__(self, master=Tk(),board=Board()):
 		self.master = master
 		self.board = board
+		
+		self.fallingTetro = None
+		self.fallingSpaces = [] #This will hold the four blocks of the current tetro
 		
 		self.master.title("Tetris")
 		self.master.geometry("300x300")
@@ -20,38 +26,60 @@ class Display:
 		for key in self.buttons:
 			self.buttons[key].pack()
 			
+		print "Adding an L tetro"
+		L = Tetro("L",self.board)
+		self.addTetro(L)
+		print "Finished adding L tetro"
 		mainloop()
 	def downPressed(self):
 		print "The user has pressed down"
+		#Check to see if there's anything below
+		for space in self.fallingSpaces:
+			oldBox = self.getBox(space)
+			newBox = self.getBox(
 	def leftPressed(self):
 		print "The user has pressed left"
 	def rightPressed(self):
 		print "The user has pressed right"
 	def endTurn(self): #This is called when a piece lands at the bottom
 		#This is not yet optimized. It is only for testing.
-		for row in range(0,self.board.height):
+		for row in range(0,self.board.depth):
 			if self.gameGrid.rowIsFull(row):
 				print "YE A ROW IS FULL"
+	#def hasBlock(self,dimensions): return self.getBox(dimensions).get()
+	def addTetro(self, tetro):
+		for startingPos in tetro.spaces:
+			currentBox = self.getBox(startingPos)
+			if currentBox.get():#If there's already something there
+				print "A tetro should have been added, but there was a box already checked."
+			currentBox.activate()
+			self.fallingSpaces.append(startingPos)
+		self.fallingTetro = tetro
+	def getBox(self, dimensions): 
+		return self.gameGrid.getBox(dimensions)
+	#def getBoxBelow(self, dimensions):
+	#	return self.gameGrid.getBox([dimensions[0],dimensions[1]
+			
 class GameGrid:
 	def __init__(self,father,master=Tk()):
 		self.master = master
 		self.father = father
-		self.boxes = [[Box(self.master) for col in range(self.father.board.width)] for row in range(self.father.board.height)]
-		for row in range(0,self.father.board.height):
+		self.boxes = [[Box(self.master) for col in range(self.father.board.width)] for row in range(self.father.board.depth)]
+		for row in range(0,self.father.board.depth):
 			for col in range(0,self.father.board.width):
 				self.boxes[row][col].grid(row,col)
 	def printGrid(self):
 		print(str(self))
 	def getBoolGrid(self):
-		boolGrid = [[False for col in range(self.father.board.width)] for row in range(self.father.board.height)]
-		for row in range(0,self.father.board.height):
+		boolGrid = [[False for col in range(self.father.board.width)] for row in range(self.father.board.depth)]
+		for row in range(0,self.father.board.depth):
 			for col in range(0,self.father.board.width):
 				boolGrid[row][col] = self.boxes[row][col].get()
 		return boolGrid
 	def __str__(self):
 		boolGrid = self.getBoolGrid()
 		s=""
-		for row in range(0,self.father.board.height):
+		for row in range(0,self.father.board.depth):
 			for col in range(0,self.father.board.width):
 				s+=str(self.boxes[row][col])
 			s+="\n"
@@ -61,6 +89,8 @@ class GameGrid:
 		for col in range(0,self.father.board.width):
 			if not boolGrid[row][col]: return False
 		return True
+	def getBox(self, dimensions):
+		return self.boxes[dimensions[1]][dimensions[0]]
 class Box:
 	def __init__(self,master):
 		self.intVar = IntVar()
@@ -77,3 +107,4 @@ class Box:
 		self.isChecked = not self.isChecked
 	def __str__(self):
 		return "#" if self.get() else "0"
+	def activate(self): self.checkBox.invoke()
